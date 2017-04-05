8a64d2340ff47dcc535748ee424a9d4df19d91a5
Zebula Sampedro <sampedro@colorado.edu>
2017-04-05 15:56:26 -0600

    Fixed XSRF failures.

--- sandstone_jupyterhub_login/handlers.py
+++ sandstone_jupyterhub_login/handlers.py
@@ -4,6 +4,10 @@ import os
 
 class JupyterHubLoginHandler(BaseHandler):
     def get(self):
+        # The XSRF token must be manually set in the absence of
+        # a web form. Accessing the property is enough to set it.
+        self.xsrf_token
+        
         api_token = os.environ['JUPYTERHUB_API_TOKEN']
 
         url = '{protocol}://{host}/hub/api/authorizations/token/{token}'.format(
