b73af6ecdb585c77aadc5e4105947202cf9d5655
Zebula Sampedro <sampedro@colorado.edu>
2017-04-05 15:02:50 -0600

    Fixed command line args for entrypoint, fixed setup.

--- sandstone_jupyterhub_login/__init__.py
+++ sandstone_jupyterhub_login/__init__.py
@@ -6,19 +6,18 @@ import sandstone
 
 def run_server():
     parser = argparse.ArgumentParser(description='Run Sandstone IDE via JupyterHub.')
-    parser.add_argument('port')
-    parser.add_argument('token')
-    parser.add_argument('cookie-name')
-    parser.add_argument('hub-host')
-    parser.add_argument('base-url')
-    parser.add_argument('hub-prefix')
-    parser.add_argument('hub-api-url')
-    parser.add_argument('ip')
-    parser.add_argument('user')
-
+    parser.add_argument('--port')
+    parser.add_argument('--token')
+    parser.add_argument('--cookie-name')
+    parser.add_argument('--hub-host')
+    parser.add_argument('--base-url')
+    parser.add_argument('--hub-prefix')
+    parser.add_argument('--hub-api-url')
+    parser.add_argument('--ip')
+    parser.add_argument('--user')
     args = parser.parse_args()
 
-    os.environ['SANDSTONE_PORT'] = args.port
-    os.environ['SANDSTONE_PREFIX'] = args.base_url
-
-    sandstone.app.main()
+    # Remove extraneous quotes from string
+    prefix = args.base_url[1:-1]
+    
+    sandstone.app.main(port=args.port,prefix=prefix)
